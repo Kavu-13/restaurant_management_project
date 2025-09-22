cf988030592e2852c2fcd9d6c0f0efd4237a2712
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-22 22:39:08 +0530

    50013

--- orders/models.py
+++ orders/models.py
@@ -6,3 +6,14 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    # example field for your Order model
+    customer_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    #new field for order status
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Orer #{self.id} - {self.customer_name}"
