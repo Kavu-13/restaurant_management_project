93281afa0341673ebcd46450083ce876ffb69d5c
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-22 22:24:00 +0530

    50015

--- orders/utils.py
+++ orders/utils.py
@@ -1,17 +1,18 @@
-import string
-import secrets
-from .models import Coupon  # assuming you have a Coupon model
+from .models immport Coupon
 
 def generate_coupon_code(length=10):
     """
     Generate a unique alphanumeric coupon code.
-    :param length: Length of the coupon code (default = 10)
+    :param length: Length of the coupon code(default = 10)
     :return: Unique coupon code string
     """
     alphabet = string.ascii_uppercase + string.digits
 
     while True:
-        code = ''.join(secrets.choice(alphabet) for _ in range(length))
+        code = ''.join(secrets.choice(alphabet) for _in range(length))
         # Ensure uniqueness in DB
         if not Coupon.objects.filter(code=code).exists():
             return code
+
+
+            
