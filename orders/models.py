5f38a8a18c1a8a84d6476111b3fbdde7a87cfe91
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-22 22:14:13 +0530

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from .utils import generate_coupon_code
 
-# Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True, blank=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = generate_coupon_code()
+        super().save(*args, **kwargs)
