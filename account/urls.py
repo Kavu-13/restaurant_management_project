0c61f7c4df464ca032a7e2fcdffa7b4b7c40e69b
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-23 19:32:35 +0530

    50498

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
+from .views import EmailCheckView
 
 urlpatterns = [
-    
+    path("check-email/", EmailCheckView.as_view(), name="check-email"),
 ]
