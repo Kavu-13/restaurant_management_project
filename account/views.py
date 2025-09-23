35aa45d393963ee7ecd1ddda1b44415271236278
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-23 19:30:02 +0530

    50498

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from utils.validation_utils import is_valid_email
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+class EmailCheckView(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        if noy is_valid_email(email):
+            return Response({"error": "Ivalid email address"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "Valid email!"}, status=status.HTTP_200_OK)
