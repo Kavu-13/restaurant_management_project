c826f828822ffca79781ea7e4eed954b61452fd6
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-23 18:19:38 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import OrderHistoryView
 from .views import *
 
 urlpatterns = [
-    
+    path("history/", OrderHistoryView.as_view(), name="order-history"),
 ]
