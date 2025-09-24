9611945ea8d76bcd6096a4466cf39d8d2b03a672
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-24 18:58:41 +0530

    50496

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import MenuCategoryListView
+from .views import MenuCategoryListView, MenuItemSearchViewSet
 
 
 menu_item_update = MenuItemVewSet.as_view({
     'put': 'update',
 })
+
+
+menu_item_search = MenuItemSearchViewSet.as_view({
+    'get': 'list',
+})
+
 urlpatterns = [
     path('categories/', MenuCategoryListView.as_view(), name='menu-categories'),
     path('menu-item/<int:pk>/', menu_item_update, name='menu-item-update'),
+    path('menu-items/search/', menu_item_search, name='menu-item-search'),
 ]
