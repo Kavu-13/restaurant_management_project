0dc53d9603a41b47a9f7675da32b218d7c5a3b10
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-24 18:16:30 +0530

    50497

--- home/models.py
+++ home/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
  class MenuCategory(models.Model):
-    name = models.CharField(max_lengt=100, unique=True)
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = odels.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_length=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
