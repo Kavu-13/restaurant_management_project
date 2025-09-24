5857770c925bbf20ebfda0f1911499ade661b9eb
Kavya Singh <142147151+Kavu-13@users.noreply.github.com>
2025-09-25 00:26:57 +0530

    51069

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import *
-from .views import EmailCheckView
+from .views import EmailCheckView, UserProfileViewSet
+
+user_profile_update = UserProfileViewSet.as_view({
+    'put': 'update',
+})
 
 urlpatterns = [
     path("check-email/", EmailCheckView.as_view(), name="check-email"),
-]
+    path('users/<int:pk>/profile/', user_profile_update, name='user-profile-update'),
+    ]
